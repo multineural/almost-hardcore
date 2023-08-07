2f9884d03940853931443b58a8c77f822a9584c8
BuildTools <unconfigured@null.spigotmc.org>
2023-08-07 12:21:27 -0600

    JE - make it harder to lose all totems, jeez

--- src/main/java/net/multineural/minecraft/PlayerEffect.java
+++ src/main/java/net/multineural/minecraft/PlayerEffect.java
@@ -11,10 +11,10 @@ public class PlayerEffect {
 
     public static int doUndyingEffect(PlayerEntity player, int numLivesToSubtract) {
 
-        player.setHealth(1.0F);
+        player.setHealth(10.0F); //TODO make these configurable
         player.canModifyBlocks();
         player.clearStatusEffects();
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 900, 1));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 800, 2));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 100, 1));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 800, 0));
         player.getWorld().sendEntityStatus(player, (byte) 35);
--- src/main/java/net/multineural/minecraft/persist/PlayerPojo.java
+++ src/main/java/net/multineural/minecraft/persist/PlayerPojo.java
@@ -2,7 +2,7 @@ package net.multineural.minecraft.persist;
 
 public class PlayerPojo {
 
-    private int numLives = 3;
+    private int numLives = 3; //TODO make this configurable
 
     public int getNumLives() {
         return numLives;
